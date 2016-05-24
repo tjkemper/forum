0cbde4c4993353c6543a04804dc964530f3181df
Taylor Kemper <taylor@revature.com>
2016-05-24 11:15:34 -0400

    add Java, JavaScript files for project

---
+++ src/main/java/com/ex/service/ForumService.java
@@ -0,0 +1,24 @@
+package com.ex.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.ex.domain.Message;
+import com.ex.domain.Room;
+import com.ex.domain.User;
+
+public interface ForumService {
+	
+	public User auth(User user);
+	public User registerUser(User user);
+	
+	public List<Room> getRooms();
+	public void createRoom(Room room);
+	public void closeRoom(String roomName);
+	
+	public List<Message> getMessagesByRoom(String roomName);
+	public Page<Message> getMessagesByRoomPage(String roomName, Integer page, Integer size);
+	public void postMessage(Message message, String roomName);
+	
+}
