20c0e5c07f6866e17292415032de3abaf1abc3a3
Taylor Kemper <taylor@revature.com>
2016-06-06 19:40:44 -0400

    add scrollToTop directive

--- src/main/webapp/WEB-INF/ng/app.js
+++ src/main/webapp/WEB-INF/ng/app.js
@@ -532,5 +532,39 @@ angular.module("ForumApp")
 	};
 });
 
+angular.module("ForumApp")
+.directive("scrollToTop", function(){
+	return {
+		scope : {
+			
+		},
+		templateUrl: "ng/templates/directives/scrollToTop.html",
+		link: function(scope, element, attrs){
+			
+			var scrollElement = angular.element(element[0].querySelector('#scroll'));
+			
+			scrollElement[0].addEventListener('click', function(event){
+				scrollTo(0,0);
+//				angular.element().animate({scrollTop: 0}, "slow");
+			});
+			
+			angular.element(document).bind('scroll', function() {
+				
+			    var currentYPos = scrollY; 						//current y position
+			    var bodyHeight = document.body.clientHeight; 	//total height of <body>
+			    var windowInnerHeight = innerHeight; 			//height of inner window
+
+			    var scrollPercentage = currentYPos / (bodyHeight - windowInnerHeight);
+
+			    var maxTop = 180;
+			    var scrollElementHeight = 30; //because
+			    var newTop = scrollPercentage * maxTop - scrollElementHeight;
+			    scrollElement[0].style.top = newTop+'px';
+			    
+			});
+		}
+	};
+});
+
 
 
