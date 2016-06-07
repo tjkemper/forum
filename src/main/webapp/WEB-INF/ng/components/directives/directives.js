a56e52e75f2dfda62dcea2e8ac6380ace1e70952
Taylor Kemper <taylor@revature.com>
2016-06-07 11:58:05 -0400

    complete slow scrollToTop

--- src/main/webapp/WEB-INF/ng/components/directives/directives.js
+++ src/main/webapp/WEB-INF/ng/components/directives/directives.js
@@ -50,11 +50,29 @@ angular.module("ForumApp")
 		templateUrl: "ng/templates/directives/scrollToTop.html",
 		link: function(scope, element, attrs){
 			
+			function scrollToTop(scrollDuration) {
+			    const   scrollHeight = window.scrollY,
+			            scrollStep = Math.PI / ( scrollDuration / 15 ),
+			            cosParameter = scrollHeight / 2;
+			    var     scrollCount = 0,
+			            scrollMargin;
+			    requestAnimationFrame(step);        
+			    function step () {
+			        setTimeout(function() {
+			            if ( window.scrollY != 0 ) {
+			                    requestAnimationFrame(step);
+			                scrollCount = scrollCount + 1;  
+			                scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+			                window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
+			            }
+			        }, 15 );
+			    }
+			}	
+		
 			var scrollElement = angular.element(element[0].querySelector('#scroll'));
 			
 			scrollElement[0].addEventListener('click', function(event){
-				scrollTo(0,0);
-//				angular.element().animate({scrollTop: 0}, "slow");
+				scrollToTop(100);
 			});
 			
 			angular.element(document).bind('scroll', function() {
