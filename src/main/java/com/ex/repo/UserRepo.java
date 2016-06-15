7017a6915e3c9da64b7298e5fa34d5504e5e11d0
Taylor Kemper <taylor@revature.com>
2016-06-15 14:49:31 -0400

    UserRepo - findOneByUsername

--- src/main/java/com/ex/repo/UserRepo.java
+++ src/main/java/com/ex/repo/UserRepo.java
@@ -16,6 +16,8 @@ public interface UserRepo extends JpaRepository<User, Integer> {
 
 	
 	List<User> findByUsername(String username);
+	User findOneByUsername(String username);
+	
 	
 //	List<User> findByUsernameAndPassword(String username, String password);
 /*	
